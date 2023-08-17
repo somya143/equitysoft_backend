01253818ad12da1956bcec2573b2e764ba3fff5c
Somya Ranjan Singh <maaheersj@gmail.com>
2023-08-17 09:31:58 +0530

    final commit

--- controller/form/form.controller.js
+++ controller/form/form.controller.js
@@ -39,6 +39,13 @@ exports.addQuestion = async (req, res, next) => {
         if (!form) {
             return res.status(404).send({ message: 'Form not found', error: true });
         }
+        form.questions.forEach((question) => {
+          const matchingQuestion = form.questions.find((q) => q._id.equals(question._id));
+          if (matchingQuestion) {
+            question.questionText = matchingQuestion.questionText;
+            question.valueType = matchingQuestion.valueType;
+          }
+        });
         // Creating a new question and save it
         const newQuestion = new Question({
             questionText,
@@ -47,7 +54,7 @@ exports.addQuestion = async (req, res, next) => {
         await newQuestion.save();
 
         // Adding the new question's ID to the form's questions array
-        form.questions.push(newQuestion._id);
+        form.questions.push(newQuestion);
         await form.save();
 
         res.send(form);
@@ -58,29 +65,33 @@ exports.addQuestion = async (req, res, next) => {
 
 // Submitting the form
 exports.submitForm = async (req, res, next) => {
-    try {
-        const { formId } = req.params;
-        const { responses } = req.body;
-    
-        // Finding the form by its ID
-        const form = await Form.findById(formId);
-        if (!form) {
-          return res.status(404).send({ message: 'Form not found', error: true });
-        }
-    
-        // Looping through the responses and adding them to the form's responses array
-        responses.forEach(async (response) => {
-          const { questionId, responseValue } = response;
-          form.responses.push({ questionId, responseValue });
-        });
-    
-        await form.save();
-    
-        res.send(form);
-      } catch (error) {
-        res.status(500).send({ message: error.message, error: true })
-      }
+  try {
+    const { formId } = req.params;
+    const { responses } = req.body;
+
+    // Finding the form by its ID
+    const form = await Form.findById(formId);
+    if (!form) {
+      return res.status(404).send({ message: 'Form not found', error: true });
+    }
+
+    // Create an array to store promises for responses
+    const responsePromises = responses.map(async (response) => {
+      const { questionId, responseValue } = response;
+      form.responses.push({ questionId, responseValue });
+    });
+
+    // Await all response promises to complete
+    await Promise.all(responsePromises);
+
+    await form.save();
+
+    res.send(form);
+  } catch (error) {
+    res.status(500).send({ message: error.message, error: true });
+  }
 };
+
 
 
 
